3cbcb0d3ecf006d3568726644c39780df50dd853
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-05-16 09:44:52 -0400

    Remove unused Publication import from Publications page to streamline code and improve maintainability.

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { publications, Publication } from './data';
+import { publications } from './data';
 import React, { useState } from 'react';
 import Link from 'next/link';
 import '../animations.css';
